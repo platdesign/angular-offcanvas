42174f151b6860e8352c92e36128d716facd1f90
Christian Blaschke <mail@platdesign.de>
2015-02-03 01:39:56 +0100

    eventhandling

--- src/directives/pdOffcanvasCover.js
+++ src/directives/pdOffcanvasCover.js
@@ -1,16 +1,36 @@
 'use strict';
 
+var $ = global.$;
+
 module.exports = ['pdOffcanvas', function(pdOffcanvas) {
 	return {
 		restrict: 'A',
-		link: function(scope, el, attrs) {
+		scope: {
+			pdOffcanvasCover: '='
+		},
+		link: function(scope, el) {
 
-			var id = attrs.pdOffcanvasCover;
+			var id = scope.pdOffcanvasCover;
 
-			pdOffcanvas.registerCover(id, el);
+			if( $.isArray(id) ) {
+				$.each(id, function(item, key) {
+					pdOffcanvas.registerCover(key, el);
+				});
+			} else {
+				pdOffcanvas.registerCover(id, el);
+			}
+
+
+			scope.$on('pdOffcanvas:show', function(event, id) {
+				pdOffcanvas.show(event, id);
+			});
+
+			scope.$on('pdOffcanvas:hide', function(event, id) {
+				pdOffcanvas.hide(event, id);
+			});
 
 			scope.$on('pdOffcanvas:toggle', function(event, id) {
-				pdOffcanvas.toggle(id);
+				pdOffcanvas.toggle(event, id);
 			});
 
 		}
--- src/services/pdOffcanvas.js
+++ src/services/pdOffcanvas.js
@@ -2,71 +2,119 @@
 
 module.exports = ['$rootScope', function($scope) {
 	var service = this;
-
 	var store = {};
 	var docEl = $(document);
-
-	this.register = function(name, el) {
-
-		var item = store[name] = store[name] || {};
-
-		item.canvasEl = el;
+	var activeItem;
+	var eventName = function(event, id) {
+		return event+'.pdOffCanvas-'+id;
 	};
 
-	this.registerCover = function(name, el) {
-		var item = store[name] = store[name] || {};
+
+
+	this.register = function(id, el) {
+
+		var item = store[id] = store[id] || {};
+
+		item.id = id;
+		item.canvasEl = el;
+		el.hide();
+
+		el.addClass('pd-offcanvas-'+id);
+	};
+
+	this.registerCover = function(id, el) {
+		var item = store[id] = store[id] || {};
 
 		item.coverEl = el;
+		el.addClass('pd-offcanvas-cover-'+id);
 	};
 
-	this.toggle = function(id) {
+
+	this.show = function(event, id) {
 		var item = store[id];
 		if(!item) { return; }
 
+		var cover = item.coverEl;
+		var canvas = item.canvasEl;
+
+		if( cover.status === true && activeItem ) {
+			// Switch only between canvas
+			// remove the old events to prevent closing the cover
+			docEl.off( eventName('keyup', activeItem.id) );
+			docEl.off( eventName('click', activeItem.id) );
+			service.hide(event, activeItem.id);
+			canvas.show();
+
+			if(activeItem !== item) {
+				$scope.$broadcast('pdOffCanvas:visible', id);
+			}
+		} else {
+			canvas.show();
+			$scope.$broadcast('pdOffCanvas:visible', id);
+		}
 
 
-		var escEventName = 'keyup.pdOffCanvas-'+id;
-		var outClickEventName = 'click.pdOffCanvas-'+id;
+		if(activeItem && activeItem.canvasEl !== canvas) {
+			activeItem.canvasEl.hide();
+		}
 
-		function eventsOn() {
-			setTimeout(function() {
-				docEl.on(escEventName, function(e) {
-					if(e.which === 27) {
-						service.toggle(id);
-					}
-				});
+		cover.addClass('pd-offcanvas-cover-move');
+		cover.addClass('pd-offcanvas-cover-move-'+id);
 
-				docEl.on(outClickEventName, function(e) {
-					var target = $(e.target);
-					if( !target.is(item.canvasEl) && !item.canvasEl.has(target).length ) {
-						e.preventDefault();
-						service.toggle(id);
-					}
-				});
+
+
+		setTimeout(function() {
+			docEl.on( eventName('keyup', id) , function(e) {
+				if(e.which === 27) {
+					service.hide(e, id);
+				}
 			});
 
-		}
+			docEl.on( eventName('click', id) , function(e) {
+				var target = $(e.target);
+				if( !target.is(item.canvasEl) && !item.canvasEl.has(target).length ) {
+					e.preventDefault();
+					service.hide(e, id);
+				}
+			});
+		});
 
-		function eventsOff() {
-			docEl.off(escEventName);
-			docEl.off(outClickEventName);
-		}
 
+		cover.status = true;
+		activeItem = item;
+	};
+
+	this.hide = function(event, id, hide) {
+		var item = store[id];
+		if(!item) { return; }
+
+		var cover = item.coverEl;
+		var canvas = item.canvasEl;
+
+
+
+		cover.removeClass('pd-offcanvas-cover-move');
+		cover.removeClass('pd-offcanvas-cover-move-'+id);
+		$scope.$broadcast('pdOffCanvas:hidden', id);
+
+		cover.status = false;
+
+		docEl.off( eventName('keyup', id) );
+		docEl.off( eventName('click', id) );
+	};
+
+
+	this.toggle = function(event, id) {
+		var item = store[id];
+		if(!item) { return; }
 
 		var cover = item.coverEl;
 
-		if(!item.status) {
-			item.status = true;
-			cover.addClass('pd-offcanvas-cover-move');
-			$scope.$broadcast('pdOffCanvas:show', id);
-			eventsOn();
-
+		if(!cover.status) {
+			service.show(event, id);
 		} else {
-			item.status = false;
-			cover.removeClass('pd-offcanvas-cover-move');
-			$scope.$broadcast('pdOffCanvas:hide', id);
-			eventsOff();
+			service.hide(event, id);
 		}
-
 	};
+
 }];
